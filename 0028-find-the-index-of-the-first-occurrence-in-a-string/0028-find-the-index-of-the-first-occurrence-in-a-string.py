c6faaee142efc266892fa4de342109ff89889c3f
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-09-27 18:52:42 +0100

    Time: 30 ms (94.52%), Space: 16 MB (98.12%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
 
+        ### 1. Naive Approach (O(nm))
         # possible positions (indices) where needle can be in the haystack
-        possible_positions = len(haystack) - len(needle) + 1
+        possible_indices = len(haystack) - len(needle) + 1
 
-        for i in range(possible_positions):
+        for i in range(possible_indices):
             for j in range(len(needle)):
                 if needle[j] != haystack[i+j]:
                     break
@@ -13,6 +14,25 @@ class Solution:
                     return i
 
         return -1
+
+        ### 2. KMP Algorithm O(m+n)
+
+        ## Create a prefix table 
+        # we find the length of the longest proper prefix that is also a suffix 
+        # for every prefix in needgle
+
+        # lps = [0] * len(needle)
+
+        # pre = 0
+        # for i in range(1, len(needle)):
+        #     while (pre > 0 and needle[i] != needle[pre]):
+        #         pre = lps[pre-1]
+        #     if needle[pre] == needle[i]:
+        #         pre += 1
+        #         lps[i] = pre
+
+
+        # # Conduct search
 
 
             
