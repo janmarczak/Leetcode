7d682849f80629b08fbee331abad8c47e1f7edf4
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-02 12:19:27 +0100

    Time: 38 ms (86.48%), Space: 16 MB (99.57%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,28 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
 
-        if x == 0:
-            return 0
-
         # 0: 0
         # 1 - 3: 1
         # 4 - 8: 2
         # 9 - 15: 3
         # 16 - 24: 4
         # ...
+        # X: 17 -> return 4
+        # 2 * 2 = 4
+        # 3 * 3
+        # 4 * 4
 
-        for n in range(1, x+1):
-            if n * n == x:
-                return n
-            elif n * n > x:
-                return n-1
+        if x == 0:
+            return 0
+
+        first, last = 1, x
+        while first <= last:
+            mid = first + (last - first) // 2 # 3
+            if mid * mid == x: 
+                return mid
+            elif mid * mid > x:
+                last = mid - 1 # 7
+            elif mid * mid < x:
+                first = mid + 1 # 4
+        return last
         
