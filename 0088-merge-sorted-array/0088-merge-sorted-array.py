9d632ac99fa6016d392f8c89495aba8fd8a4f5d2
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-02 13:40:30 +0100

    Time: 41 ms (75.4%), Space: 16.1 MB (94.18%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,23 +4,28 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        # Input: nums1 = [2,5,6,0,0,0], m = 3, nums2 = [1,2,7], n = 3
-        # Output: [1,2,2,5,6,7]
+        # Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+        # Output: [1,2,2,3,5,6]
+
+        # 1 - [1,2,3,0,0,6]
+        # 2 - [1,2,3,0,5,6]
+        # 3 - [1,2,3,3,5,6]
 
         # 1st: Append and sort:
-        nums1[m:len(nums1)] = nums2
-        nums1.sort()
+        # nums1[m:len(nums1)] = nums2 #O(n) n-> length of nums2
+        # nums1.sort() # O(m+n log(m+n)) -> same as NlogN but n is the length of merged array
 
-        # j = 0
-        # for i in range(len(nums1)):
-        #     print(nums1, nums2)
-        #     print(i, j)
-        #     if nums2[j] <= nums1[i]:
-        #         nums1[i+1:m+1] = nums1[i:m]
-        #         nums1[i] = nums2[j]
-        #         j += 1
-        #         print(nums1)
-        #     elif i > m:
-        #         nums1[i:] = nums2[j:]
-        #         print(nums1, nums2)
-        #         return
+        # 2nd: Using pointers:
+        # Pointers of last elements in our arrays
+        i, j, k = m-1, n-1, m+n-1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+
