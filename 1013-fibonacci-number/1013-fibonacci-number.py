60f5d9d2eb80532199f7957583b4c0a96b3c9f4f
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-02 12:53:19 +0100

    Time: 585 ms (7.23%), Space: 16.1 MB (96.81%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,7 +2,19 @@ class Solution:
     def fib(self, n: int) -> int:
 
         # 1st solution recursive:
-        if n <=1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        # if n <=1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
+    
+        cache = {
+                0: 0,
+                1: 1,
+            }
+        return self.recursive_helper(n, cache)
         
+    # 2nd Memoization:
+    def recursive_helper(self, n: int, cache) -> int:
+        if n in cache:
+            return cache[n]
+        return self.recursive_helper(n-1, cache) + self.recursive_helper(n-2, cache)
+
