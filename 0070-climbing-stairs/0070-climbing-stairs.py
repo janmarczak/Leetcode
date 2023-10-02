eaa6c2ae16c3d89c0fa20d4b95ed1af56f43c3e5
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-02 12:47:35 +0100

    Time: 28 ms (97.12%), Space: 16.1 MB (83.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,5 @@
 class Solution:
-    def climbStairs(self, n: int, cache=None) -> int:
+    def climbStairs(self, n: int) -> int:
 
         # First solution: Too long
         # if n <= 0:
@@ -11,21 +11,22 @@ class Solution:
         #     results2 = self.climbStairs(n - 2) 
         # return results1 + results2
 
+
         # Second Solution: Memoisation
-        if cache is None:
-            cache = {
+        def recursive(n: int, cache=dict[int, int]) -> int:
+            if n in cache:
+                return cache[n]
+            cache[n] = recursive(n - 1, cache) + recursive(n - 2, cache)
+            return cache[n]
+
+        cache = {
                 0: 0,
                 1: 1,
                 2: 2,
             }
+        return recursive(n, cache)
 
-        if n in cache:
-            return cache[n]
-        cache[n] = self.climbStairs(n - 1, cache) + self.climbStairs(n - 2, cache)
-        return cache[n]
-
-
-        ##### 5
+        ##### EXAMPLE N:5
         # 1. r1 = climbStairs(4)
         #   1.1 r1 = climbStairs(3)
         #       1.1.1 r1 = climbStairs(2)
