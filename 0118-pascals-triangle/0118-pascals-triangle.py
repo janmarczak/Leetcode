11b316a848e40aeccb3e7e36acb28d2c0d4c9f80
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-17 10:50:55 +0200

    Time: 34 ms (90.92%), Space: 16.1 MB (89.04%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -2,41 +2,36 @@ class Solution:
     solution = []
     def generate(self, numRows: int) -> List[List[int]]:
 
-        solution = []
-        for i in range(1, numRows+1):
-            if i == 1 or i == 2:
-                solution.append(([1] * i))
-            else:
-                tmp = [1]
-                for j in range(1, i-1): # range: 1, 1
-                    tmp.append(solution[i-2][j-1] + solution[i-2][j])
-                tmp.append(1)
-                solution.append(tmp)
+        # For Loop Solution
+        # solution = []
+        # for i in range(1, numRows+1):
+        #     if i == 1 or i == 2:
+        #         solution.append(([1] * i))
+        #     else:
+        #         tmp = [1]
+        #         for j in range(1, i-1): # range: 1, 1
+        #             tmp.append(solution[i-2][j-1] + solution[i-2][j])
+        #         tmp.append(1)
+        #         solution.append(tmp)
 
-        return solution
-
-
-
-        # if numRows == 1:
-        #     return [[1]]
-        # elif numRows == 2:
-        #     return [[1, 1]]
-        # else:
-
-        #     # 4
-        #     [1,3,3,1]
-
-        #     # 5
-        #     [1,4,6,4,1]
+        # return solution
 
 
-        #     return solution + generate()
+        # Dynamic solution
+        if numRows == 0:
+            return []
+        if numRows == 1:
+            return [[1]]
 
-            # 5 -> 5 integer array
-            # 1
-            # 1+3
-            # 3+3
-            # 3+1
-            # 1
+        prev_rows = self.generate(numRows - 1)
+        prev_row = prev_rows[-1]
+        current_row = [1]
 
-        
+        for i in range(1, numRows - 1):
+            current_row.append(prev_row[i-1] + prev_row[i])
+
+        current_row.append(1)
+        prev_rows.append(current_row)
+
+        return prev_rows
+
