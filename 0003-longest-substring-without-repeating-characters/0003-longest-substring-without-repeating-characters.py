01cb38652a4e5e9f2d304d9d03f4ae7d0eb6760c
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-10-17 11:59:19 +0200

    Time: 57 ms (75.08%), Space: 16.4 MB (60.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -10,71 +10,15 @@ class Solution:
 
             # if the current char is not in the map or its index is less than left, it is a new char
             if s[right] not in indices or indices[s[right]] < left:
+                # the indices[s[right]] < left check is there to account for 
+                # repeating chars that were before the initial repeating char
+                # ex: tmmzuxt (first t when checking the last t)
                 indices[s[right]] = right
                 max_length = max(max_length, right - left + 1)
             else:
-                # char is repeating so we move the left to the next position after the last occurance of the char
+                # char is repeating so left is the position after the last occurance of the char
                 left = indices[s[right]] + 1
-                indices[s[right]] = right # update the index
-
-            # if s[i] not in indices:
-            #     indices[s[i]] = i
-            #     length += 1
-            # else:
-            #     max_length = max(length, max_length)
-            #     length -= (indices[s[i]])
-            #     indices = {}
-            #     indices[s[i]] = i
-
-        # abcabcbb
-        # l=1, ml=0, {a: 0}
-        # l=2, ml=0, {a: 0, b: 1}
-        # l=3, ml=0, {a: 0, b: 1, c:2}
-        # l=3, ml=3, {a: 0, b: 1, c:2}
-                
-
-        # tmmzuxt
-        # l=1 , ml=, {t: 0}
-        # l=2 , ml=, {t: 0, m: 1}
-        # l=1 , ml=2, {t: 0, m: 2}
-        # l=2 , ml=2, {t: 0, m: 2, z: 3}
-        # l=3 , ml=2, {t: 0, m: 2, z: 3, u: 4}
-        # l=4 , ml=2, {t: 0, m: 2, z: 3, u: 4, x: 5}
-        # l=4 , ml=2, {t: 0, m: 2, z: 3, u: 4, x: 5}
-
-        # pwwkew
-        # {p: 0}, l=1, ml=0
-        # {p: 0, w: 1}, l=2, ml=0
-        # {p: 0, w: 2}, l=1, ml=2
-        # {p: 0, w: 2, k: 3}, l=2, ml=2
-        # {p: 0, w: 2, k: 3, e: 4}, l=3, ml=2
-        # {p: 0, w: 2, k: 3, e: 4}, l=3, ml=3
+                indices[s[right]] = right
             
-
         return max_length
-                # a: 0
-                # b: 1
-                # c: 2
-                # length = 3
 
-        
-
-
-        # abcadb
-
-        # bcad -> 4
-
-        # abcbda
-
-        # a: 0
-        # b: 1
-        # c: 2
-
-        # abcbad
-
-        # a: 0
-        # b: 1
-        # c: 2
-        # 
-
-        # HashMap of character to indices
