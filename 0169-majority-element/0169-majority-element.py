743173c938a15d1552b3529302f8305dbb7403de
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-11-01 15:44:19 +0000

    Time: 166 ms (8.03%), Space: 17.8 MB (76.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,33 +3,46 @@ class Solution:
 
         # 1. HashMap
 
-        # freq = {}
-        # for n in nums:
-        #     if n not in nums:
-        #         freq[n] = 1
-        #     else:
-        #         freq[n] += 1
+        freq = {}
+        for n in nums:
+            if n not in freq:
+                freq[n] = 1
+            else:
+                freq[n] += 1
+                
+            if freq[n] > (len(nums) / 2):
+                return n
 
-        # [2,2,1,1,1,2,2]
+
+        # max_value = -1
+        # majority_element = None
+        # for key, value in freq.items():
+        #     if value > max_value:
+        #         majority_element = key
+        #         max_value = value
+
+        # return majority_element
+
+        # We know the length
 
         # 1. Sort the array
-        nums = sorted(nums)
-        max_count = 1
-        majority_element = nums[0]
-        count = 1
-        element = nums[0]
-        for i in range(1, len(nums)):
-            print(nums[i])
-            if nums[i] == element:
-                count += 1
-            else:
-                element = nums[i]
-                count = 1
+        # nums = sorted(nums)
+        # max_count = 1
+        # majority_element = nums[0]
+        # count = 1
+        # element = nums[0]
+        # for i in range(1, len(nums)):
+        #     print(nums[i])
+        #     if nums[i] == element:
+        #         count += 1
+        #     else:
+        #         element = nums[i]
+        #         count = 1
 
-            if count > max_count:
-                    max_count = count
-                    majority_element = element
-        return majority_element
+        #     if count > max_count:
+        #             max_count = count
+        #             majority_element = element
+        # return majority_element
 
 
         
