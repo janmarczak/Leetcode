402b0a0c61f58801a88ca2f8c34d2d5430dcf59d
Janek Marczak <60696500+janmarczak@users.noreply.github.com>
2023-11-01 12:03:49 +0000

    Time: 124 ms (60.11%), Space: 18.8 MB (81.06%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,20 +1,40 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
-        # Input: nums = [4,1,2,1,2]
-        # Output: 4
 
-        freq = {}
+        ###### 1. Frequency Map
+        # Time: O(n)
+        # Space: O(n)
 
+        # freq = {}
+
+        # for element in nums:
+        #     if element not in freq:
+        #         freq[element] = 1
+        #     else:
+        #         freq[element] += 1
+
+        # for key, value in freq.items():
+        #     if value == 1:
+        #         return key
+
+        ###### 2. XOR
+        # Time: O(N)
+        # Space: O(1)
+        # Elements with frequency 2 will result in 0. 
+        # And the only element with frequency 1 will generate the answer (itself)
+
+        # A	B	A⊕B
+        # 1	1	0
+        # 0	1	1
+        # 1	0	1
+        # 0	0	0
+
+        answer = 0
         for element in nums:
-            if element not in freq:
-                freq[element] = 1
-            else:
-                freq[element] += 1
-        
-        print(freq)
+            answer = answer ^ element
+        return answer
 
-        for key, value in freq.items():
-            if value == 1:
-                return key
+
+
 
